97e78fc098f0db928eab7c1ca0a992b65c011148
Hewitt Wang <wangh3@bxscience.edu>
2024-05-15 10:18:29 -0400

    awfifla

--- src/core/serde.c
+++ src/core/serde.c
@@ -6,31 +6,31 @@ bool GDF_SerializeMap(GDF_Map* map, char* out_buf)
     {
         if (map->entries[i] == NULL)
             continue;
-        char line_buff[200];
-        char key_buff[150];
-        char val_buff[50];
+        char line_buf[200];
+        char key_buf[150];
+        char val_buf[50];
         
-        GDF_MKEY_ToString(i, key_buff);
+        GDF_MKEY_ToString(i, key_buf);
         switch (map->entries[i]->dtype)
         {
             case GDF_MAP_DTYPE_FLOAT:
             {
-                sprintf(val_buff, "%f", *(float*)(map->entries[i]->value));
+                sprintf(val_buf, "%f", *(float*)(map->entries[i]->value));
                 break;
             }
             case GDF_MAP_DTYPE_INT:
             {
-                sprintf(val_buff, "%d", *(int*)(map->entries[i]->value));
+                sprintf(val_buf, "%d", *(int*)(map->entries[i]->value));
                 break;
             }
             case GDF_MAP_DTYPE_BOOL:
             {
-                sprintf(val_buff, *(bool*)(map->entries[i]->value) ? "true" : "false");
+                sprintf(val_buf, *(bool*)(map->entries[i]->value) ? "true" : "false");
                 break;
             }
             case GDF_MAP_DTYPE_STRING:
             {
-                sprintf(val_buff, "%s", *(char*)(map->entries[i]->value));
+                sprintf(val_buf, "%s", *(char*)(map->entries[i]->value));
                 break;
             }
             case GDF_MAP_DTYPE_MAP:
@@ -40,22 +40,28 @@ bool GDF_SerializeMap(GDF_Map* map, char* out_buf)
             }
         } 
 
-        snprintf(line_buff, 200, "%s=%s\n", key_buff, val_buff);
-        strcat(out_buf, line_buff);
+        snprintf(line_buf, 200, "%s=%s\n", key_buf, val_buf);
+        strcat(out_buf, line_buf);
     }
 }
 
 bool GDF_DeserializeToMap(char* data, GDF_Map* out_map)
 {
-    char line_buf[512];
+    char line_buf[200];    
+    char key_buf[150];
+    char val_buf[50];
 
     // iterate through lines
-    char * line = strtok(strdup(data), "\n");
+    char* line = strtok(strdup(data), "\n");
     while(line) {
-        LOG_DEBUG("%s", line);
+        sscanf(line, "%[^=]=%s", key_buf, val_buf);
+        LOG_DEBUG("key: %s", key_buf);
+        LOG_DEBUG("value: %s", val_buf);
         line = strtok(NULL, "\n");
-    }
 
+        // TODO! get dtype, convert to said dtype then to 
+        // void* and memcpy soemthing something 
+    }
     return 0;
 }
 // max write capacity of 1mb
